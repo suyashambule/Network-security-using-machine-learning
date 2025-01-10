7a65d08e25e636390c3e41929bd245f8d12ad87f
suyash <suyashambule1234@gmail.com>
2025-01-11 04:13:38 +0530

    model trainer.py

--- main.py
+++ main.py
@@ -5,16 +5,21 @@ from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
-import sys,os
 
-if __name__ == '__main__':
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
+ 
+
+import sys
+
+if __name__=='__main__':
     try:
-        trainingpipelineconfig = TrainingPipelineConfig()
-        dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
-        data_ingestion = DataIngestion(dataingestionconfig)
+        trainingpipelineconfig=TrainingPipelineConfig()
+        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion=DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
-        dataingestionartifact = data_ingestion.initiate_data_ingestion()
-        logging.info("Data Ingestion Completed")
+        dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
         print(dataingestionartifact)
         data_validation_config=DataValidationConfig(trainingpipelineconfig)
         data_validation=DataValidation(dataingestionartifact,data_validation_config)
@@ -29,9 +34,14 @@ if __name__ == '__main__':
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
 
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
+        
+        
         
     except Exception as e:
-        
-        error_message = str(e)
-        error_details = sys
-        raise NetworkSecurityException(error_message, error_details)
+           raise NetworkSecurityException(e,sys)
